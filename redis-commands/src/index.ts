6c4df0e49f16f1c3cda78b56113efa6420992d71
Aman Kumar <amankarguwal0@gmail.com>
2024-11-14 12:57:22 +0530

    Add index.ts to initialize Redis client and execute commands

---
+++ redis-commands/src/index.ts
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import { createClient } from "redis";
+import hashes from "./hashes";
+import list from "./list";
+import set from "./set";
+import sortedSet from "./sortedSet";
+import string from "./string";
+
+export const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: 14966,
+  },
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+const runRedis = async () => {
+  await client.connect();
+  console.log("Connected to Redis");
+  console.log("=====================================");
+
+  /////////////////////////////////////////////
+
+  // Working area //
+
+  await string();
+  await list();
+  await set();
+  await sortedSet();
+  await hashes();
+
+  // Delete everything
+  //  const res = await client.flushAll();
+  //  console.log(res);
+
+  /////////////////////////////////////////////
+
+  return client.quit();
+};
+
+runRedis()
+  .then(() => {
+    console.log("=====================================");
+    console.log("Disconnected from Redis");
+  })
+  .catch((err) => {
+    console.error("Error", err);
+    client.quit();
+  });
